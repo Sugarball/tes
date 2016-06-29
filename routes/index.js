b15db3c85c651b1c147c1094dd262e25bc685282
顾俊 <jayden@yzlpie.com>
2016-06-29 15:37:40 +0800

    short url

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var moment = require('moment');
 
+var urlList = [];
+
 /* GET home page. */
 router.get('/timestamp/:time', function(req, res, next) {
 	var myDate;
@@ -36,9 +38,43 @@ router.get('/getip', function(req, res, next) {
 });
 
 
+router.get('/shorturl/:url*', function(req, res, next) {
+	// req.params res.json({unix: null,natural: null});
+	var url = req.url.slice(10);
+	if(validateURL(url)){
+		if(urlList.length > 100)urlList = [];
+		urlList.push(url);
+		res.json({
+			original_url: url,
+			natural: "https://timestamp-sugarball.herokuapp.com/gourl/" + (urlList.length -1)
+		})
+	}else{
+		res.json({error : "invalid url"});
+	}
+});
+
+router.get('/shorturl', function(req, res, next) {
+  res.render('shorturl');
+});
+
+router.get('/gourl/:index', function(req, res, next) {
+	if(parseInt(req.params.index) < urlList.length){
+		res.redirect(urlList[req.params.index]);
+	}else{
+		res.json({error : "no shoturl saved"});
+	}
+  
+});
+
+
 router.get('/', function(req, res, next) {
 
   res.render('timestamp', { title: 'Express' });
 });
+
+function validateURL(url) {
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+}
 
 module.exports = router;
